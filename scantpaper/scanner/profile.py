f861dd6bd6bf23671cbb283b50404317b1e9a37a
Jeffrey Ratcliffe <jffry@posteo.net>
2023-02-04 00:00:00 +0000

    + scantpaper/scanner/profile.py

---
+++ scantpaper/scanner/profile.py
@@ -0,0 +1,205 @@
+"Data and methods for profiles of scan options"
+
+from copy import deepcopy
+import uuid
+from gi.repository import GObject
+
+
+class Profile(GObject.Object):
+    """Have to subclass Glib::Object to be able to name it as an object in
+    Glib::ParamSpec->object in Gscan2pdf::Dialog::Scan"""
+
+    frontend = None
+    backend = None
+
+    def __init__(self, frontend=None, backend=None):
+        super().__init__()
+        if frontend is None:
+            self.frontend = {}
+        else:
+            self.frontend = deepcopy(frontend)
+        if backend is None:
+            self.backend = []
+        else:
+            self.backend = deepcopy(backend)
+            self.map_from_cli()
+
+        # add uuid to identify later which callback has finished
+        self.uuid = str(uuid.uuid1())
+
+    def add_backend_option(self, name, val, oldval=None):
+        """the oldval option is a hack to allow us not to apply geometry options
+        if setting paper as part of a profile"""
+        if name is None or name == "":
+            raise ValueError("Error: no option name")
+
+        if (oldval is not None) and val == oldval:
+            return
+        self.backend.append((name, val))
+
+        # Note any duplicate options, keeping only the last entry.
+        seen = {}
+        for i in self.each_backend_option(True):
+            (nam, _value) = self.get_backend_option_by_index(i)
+            synonyms = _synonyms(nam)
+            for key in synonyms:
+                if key in seen:
+                    self.remove_backend_option_by_index(i)
+                    break
+                seen[key] = True
+
+        self.uuid = str(uuid.uuid1())
+
+    def get_backend_option_by_index(self, i):
+        "get_backend_option_by_index"
+        return self.backend[i]
+
+    def remove_backend_option_by_index(self, i):
+        "remove_backend_option_by_index"
+        del self.backend[i]
+        self.uuid = str(uuid.uuid1())
+
+    def remove_backend_option_by_name(self, name):
+        "remove_backend_option_by_name"
+        i = None
+        for i in self.each_backend_option():
+            (key, _val) = self.get_backend_option_by_index(i)
+            if key == name:
+                break
+
+        if i <= self.num_backend_options():
+            del self.backend[i]
+
+        self.uuid = str(uuid.uuid1())
+
+    def each_backend_option(self, backwards=False):
+        """an iterator for backend options"""
+        i = len(self.backend) - 1 if backwards else 0
+        while -1 < i < len(self.backend):
+            yield i
+            i = i - 1 if backwards else i + 1
+
+    def num_backend_options(self):
+        "num_backend_options"
+        return len(self.backend)
+
+    def add_frontend_option(self, name, val):
+        "add_frontend_option"
+        if name is None or name == "":
+            raise ValueError("Error: no option name")
+
+        self.frontend[name] = val
+        self.uuid = str(uuid.uuid1())
+
+    def each_frontend_option(self):
+        "an iterator for frontend options"
+        for key in self.frontend.keys():
+            yield key
+
+    def get_frontend_option(self, name):
+        "get_frontend_option"
+        return self.frontend[name]
+
+    def remove_frontend_option(self, name):
+        "remove_frontend_option"
+        del self.frontend[name]
+
+    def get(self):
+        "return a dict of frontend and backend options"
+        return {"frontend": self.frontend, "backend": self.backend}
+
+    def map_from_cli(self):
+        """Map scanimage and scanadf (CLI) geometry options to the backend geometry names"""
+        new = Profile()
+        for i in self.each_backend_option():
+            (name, val) = self.get_backend_option_by_index(i)
+            if name == "l":
+                new.add_backend_option("tl-x", val)
+
+            elif name == "t":
+                new.add_backend_option("tl-y", val)
+
+            elif name == "x":
+                _l = self.get_option_by_name("l")
+                if _l is None:
+                    _l = self.get_option_by_name("tl-x")
+                if _l is not None:
+                    val += _l
+                new.add_backend_option("br-x", val)
+
+            elif name == "y":
+                _t = self.get_option_by_name("t")
+                if _t is None:
+                    _t = self.get_option_by_name("tl-y")
+                if _t is not None:
+                    val += _t
+                new.add_backend_option("br-y", val)
+
+            else:
+                new.add_backend_option(name, val)
+        self.backend = deepcopy(new.backend)
+
+    def map_to_cli(self, options):
+        """Map backend geometry options to the scanimage and scanadf (CLI) geometry names"""
+        new = Profile()
+        for i in self.each_backend_option():
+            (name, val) = self.get_backend_option_by_index(i)
+            if name == "tl-x":
+                new.add_backend_option("l", val)
+
+            elif name == "tl-y":
+                new.add_backend_option("t", val)
+
+            elif name == "br-x":
+                _l = self.get_option_by_name("l")
+                if _l is None:
+                    _l = self.get_option_by_name("tl-x")
+                if _l is not None:
+                    val -= _l
+                new.add_backend_option("x", val)
+
+            elif name == "br-y":
+                _t = self.get_option_by_name("t")
+                if _t is None:
+                    _t = self.get_option_by_name("tl-y")
+                if _t is not None:
+                    val -= _t
+                new.add_backend_option("y", val)
+
+            else:
+                if options is not None:
+                    opt = options.by_name(name)
+                    if "type" in opt and opt["type"] == "TYPE_BOOL":
+                        val = "yes" if val else "no"
+
+                new.add_backend_option(name, val)
+
+        new.frontend = deepcopy(self.frontend)
+
+        return new
+
+    def get_option_by_name(self, name):
+        """Extract a option value from a profile"""
+        for i in self.each_backend_option():
+            (key, val) = self.get_backend_option_by_index(i)
+            if key == name:
+                return val
+
+        return None
+
+
+def _synonyms(name):
+
+    synonyms = [
+        ["page-height", "pageheight"],
+        ["page-width", "pagewidth"],
+        ["tl-x", "l"],
+        ["tl-y", "t"],
+        ["br-x", "x"],
+        ["br-y", "y"],
+    ]
+    for synonym in synonyms:
+        if name in synonym:
+            return synonym
+
+    return [name]
---
+++ scantpaper/tests/test_03_scanner_profile.py
@@ -0,0 +1,111 @@
+"test scanner option profiles"
+from scanner.profile import Profile, _synonyms
+import pytest
+
+
+def test_1():
+    "test scanner option profiles"
+    assert _synonyms("page-height") == [
+        "page-height",
+        "pageheight",
+    ], "synonyms for SANE_NAME_PAGE_HEIGHT"
+    assert _synonyms("pageheight") == [
+        "page-height",
+        "pageheight",
+    ], "synonyms for pageheight"
+    assert _synonyms("page-width") == [
+        "page-width",
+        "pagewidth",
+    ], "synonyms for SANE_NAME_PAGE_WIDTH"
+    assert _synonyms("pagewidth") == [
+        "page-width",
+        "pagewidth",
+    ], "synonyms for pagewidth"
+    assert _synonyms("tl-x") == ["tl-x", "l"], "synonyms for SANE_NAME_SCAN_TL_X"
+    assert _synonyms("l") == ["tl-x", "l"], "synonyms for l"
+    assert _synonyms("tl-y") == ["tl-y", "t"], "synonyms for SANE_NAME_SCAN_TL_Y"
+    assert _synonyms("t") == ["tl-y", "t"], "synonyms for t"
+    assert _synonyms("br-x") == ["br-x", "x"], "synonyms for SANE_NAME_SCAN_BR_X"
+    assert _synonyms("x") == ["br-x", "x"], "synonyms for x"
+    assert _synonyms("br-y") == ["br-y", "y"], "synonyms for SANE_NAME_SCAN_BR_Y"
+    assert _synonyms("y") == ["br-y", "y"], "synonyms for y"
+
+    profile = Profile()
+    assert isinstance(profile, Profile)
+    profile.add_backend_option("y", "297")
+    assert profile.get() == {
+        "backend": [("y", "297")],
+        "frontend": {},
+    }, "basic functionality add_backend_option"
+
+    #########################
+
+    profile.add_backend_option("br-y", "297")
+    assert profile.get() == {
+        "backend": [("br-y", "297")],
+        "frontend": {},
+    }, "pruned duplicate"
+
+    #########################
+
+    profile.add_frontend_option("num_pages", 0)
+    assert profile.get() == {
+        "backend": [("br-y", "297")],
+        "frontend": {"num_pages": 0},
+    }, "basic functionality add_frontend_option"
+
+    #########################
+
+    profile = Profile(frontend={"num_pages": 1}, backend=[("br-x", "297")])
+    assert profile.get() == {
+        "backend": [("br-x", "297")],
+        "frontend": {"num_pages": 1},
+    }, "basic functionality new_from_data"
+
+    #########################
+
+    itr = profile.each_frontend_option()
+    assert next(itr) == "num_pages", "basic functionality each_frontend_option"
+    assert (
+        profile.get_frontend_option("num_pages") == 1
+    ), "basic functionality get_frontend_option"
+    with pytest.raises(StopIteration):
+        next(itr)
+
+    #########################
+
+    profile = Profile(backend=[("l", 1), ("y", 50), ("x", 50), ("t", 2)])
+    assert profile.get() == {
+        "backend": [("tl-x", 1), ("br-y", 52), ("br-x", 51), ("tl-y", 2)],
+        "frontend": {},
+    }, "basic functionality map_from_cli"
+
+    #########################
+
+    itr = profile.each_backend_option()
+    assert next(itr) == 0, "basic functionality each_backend_option"
+    assert profile.get_backend_option_by_index(0) == (
+        "tl-x",
+        1,
+    ), "basic functionality get_backend_option_by_index"
+    for _ in range(1, 4):
+        next(itr)
+    with pytest.raises(StopIteration):
+        next(itr)
+
+    #########################
+
+    itr = profile.each_backend_option(True)
+    assert next(itr) == 3, "basic functionality each_backend_option reverse"
+    for _ in range(1, 4):
+        next(itr)
+    with pytest.raises(StopIteration):
+        next(itr)
+
+    #########################
+
+    profile.remove_backend_option_by_name("tl-x")
+    assert profile.get() == {
+        "backend": [("br-y", 52), ("br-x", 51), ("tl-y", 2)],
+        "frontend": {},
+    }, "basic functionality remove_backend_option_by_name"
