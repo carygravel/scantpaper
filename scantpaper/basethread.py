7f7908a884713f5de2e52c0a893d3b2ec9a43dea
Jeffrey Ratcliffe <jffry@posteo.net>
2023-02-18 00:00:00 +0000

    BaseThread: improved error handling

--- scantpaper/basethread.py
+++ scantpaper/basethread.py
@@ -3,13 +3,15 @@ import threading
 import queue
 import collections
 from enum import Enum
+import uuid
 from gi.repository import GLib
 
-Request = collections.namedtuple("Request", ["event", "args"])
+Request = collections.namedtuple("Request", ["event", "uuid", "args"])
 Response = collections.namedtuple(
     "Response", ["type", "process", "uuid", "info", "status"]
 )
-ResponseType = Enum("ResponseType", ["FINISHED", "CANCELLED", "ERROR"])
+ResponseTypes = ["STARTED", "FINISHED", "CANCELLED", "ERROR"]
+ResponseType = Enum("ResponseType", ResponseTypes)
 
 
 class BaseThread(threading.Thread):
@@ -17,31 +19,101 @@ class BaseThread(threading.Thread):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.daemon = True
         self.requests = queue.Queue()
         self.responses = queue.Queue()
+        self.callbacks = {}
 
-    def send(self, event, *args, finished_callback=None):
+    def do_quit(self):
+        "quit function does nothing"
+
+    def send(
+        self,
+        event,
+        *args,
+        started_callback=None,
+        finished_callback=None,
+        error_callback=None,
+    ):
         "Puts the event and args as a `Request` on the requests queue"
-        request = Request(event, args)
+        request = Request(event, uuid.uuid1(), args)
+        self.callbacks[request.uuid] = {
+            "started_callback": started_callback,
+            "finished_callback": finished_callback,
+            "error_callback": error_callback,
+        }
         self.requests.put(request)
-        GLib.timeout_add(100, self._monitor_thread, finished_callback)
+        GLib.timeout_add(100, self.monitor)
 
     def run(self):
         while True:
-            event, args = self.requests.get()
-            if event == "quit":
-                break
+            event, uid, args = self.requests.get()
+            self.responses.put(
+                Response(
+                    type=ResponseType.STARTED,
+                    process=event,
+                    info=None,
+                    uuid=uid,
+                    status=None,
+                )
+            )
             handler = getattr(self, f"do_{event}", None)
-            if not handler:
-                raise NotImplementedError(f"Process has no handler for [{event}]")
-            request = handler(*args)
-            self.responses.put(request)
+            if handler is None:
+                self.responses.put(
+                    Response(
+                        type=ResponseType.ERROR,
+                        process=event,
+                        info=None,
+                        uuid=uid,
+                        status=f"no handler for [{event}]",
+                    )
+                )
+            else:
+                try:
+                    self.responses.put(
+                        Response(
+                            type=ResponseType.FINISHED,
+                            process=event,
+                            info=handler(*args),
+                            uuid=uid,
+                            status=None,
+                        )
+                    )
+                    if event == "quit":
+                        break
+                except Exception as err:  # pylint: disable=broad-except
+                    self.responses.put(
+                        Response(
+                            type=ResponseType.ERROR,
+                            process=event,
+                            info=None,
+                            uuid=uid,
+                            status=str(err),
+                        )
+                    )
+            self.requests.task_done()
 
-    def _monitor_thread(self, finished_callback):
+    def monitor(self, block=False):
+        "monitor the thread, triggering callbacks as required"
+        if not block:
+            # no point in returning if there are still responses
+            while not self.responses.empty():
+                return self._monitor_response(block)
+        else:
+            return self._monitor_response(block)
+
+    def _monitor_response(self, block=False):
         try:
-            result = self.responses.get(False)
-            if finished_callback is not None:
-                finished_callback(result)
+            result = self.responses.get(block)
         except queue.Empty:
             return GLib.SOURCE_CONTINUE
+        callback = ResponseTypes[result.type.value - 1].lower() + "_callback"
+        if (
+            result.uuid in self.callbacks
+            and callback in self.callbacks[result.uuid]
+            and self.callbacks[result.uuid][callback] is not None
+        ):
+            self.callbacks[result.uuid][callback](result)
+            if callback in ["finished_callback", "error_callback"]:
+                del self.callbacks[result.uuid]
         return GLib.SOURCE_REMOVE
--- scantpaper/tests/test_083_basethread.py
+++ scantpaper/tests/test_083_basethread.py
@@ -1,36 +1,68 @@
 "test basethread class"
 from basethread import BaseThread, Response, ResponseType
-import gi
-
-gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk  # pylint: disable=wrong-import-position
 
 
 class MyThread(BaseThread):
     "test thread class"
 
-    def do_sum(self, arg1, arg2):
+    response_counter = 0
+
+    def do_div(self, arg1, arg2):  # pylint: disable=no-self-use
         "test method"
-        return Response(
-            type=ResponseType.FINISHED,
-            process="sum",
-            info=arg1 + arg2,
-            uuid="",
-            status=None,
+        return arg1 / arg2
+
+    def callback(self, response):
+        "callback"
+        print(
+            f"in callback with response {response} response_counter {self.response_counter}",
+            flush=True,
         )
+        assert response._replace(uuid="") == EXPECTED[self.response_counter], str(
+            self.response_counter
+        )
+        self.response_counter += 1
 
 
-def finished_callback(result):
-    "finished callback"
-    assert result.info == 3, "basic two-way functionality"
-    Gtk.main_quit()
+EXPECTED = [
+    Response(type=ResponseType.STARTED, process="div", uuid="", info=None, status=None),
+    Response(type=ResponseType.FINISHED, process="div", uuid="", info=0.5, status=None),
+    Response(
+        type=ResponseType.ERROR,
+        process="div",
+        uuid="",
+        info=None,
+        status="division by zero",
+    ),
+    Response(
+        type=ResponseType.ERROR,
+        process="nodiv",
+        uuid="",
+        info=None,
+        status="no handler for [nodiv]",
+    ),
+]
 
 
 def test_1():
     "test baseprocess class"
     thread = MyThread()
-    thread.daemon = True
     thread.start()
-    thread.send("sum", 1, 2, finished_callback=finished_callback)
+    thread.send(
+        "div", 1, 2, started_callback=thread.callback, finished_callback=thread.callback
+    )
+    thread.monitor(block=True)  # for started_callback
+    assert thread.response_counter == 1, "checked all expected responses #1"
+    thread.monitor(block=True)  # for finished_callback
+    assert thread.response_counter == 2, "checked all expected responses #2"
+
+    thread.send("div", 1, 0, error_callback=thread.callback)
+    thread.monitor(block=True)  # for started_callback
+    thread.monitor(block=True)  # for error_callback
+    assert thread.response_counter == 3, "checked all expected responses #3"
+
+    thread.send("nodiv", 1, 2, error_callback=thread.callback)
+    thread.monitor(block=True)  # for started_callback
+    thread.monitor(block=True)  # for error_callback
+    assert thread.response_counter == 4, "checked all expected responses #4"
+
     thread.send("quit")
-    Gtk.main()
