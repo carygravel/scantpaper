f5316b3864d95e86c208bee18d130b4d58097604
Jeffrey Ratcliffe <jffry@posteo.net>
2023-04-12 00:00:00 +0000

    + SimpleList

---
+++ scantpaper/simplelist.py
@@ -0,0 +1,314 @@
+"A simple interface to Gtk's complex MVC list widget"
+from warnings import warn
+from gi.repository import Gtk
+
+
+def scalar_cell_renderer(_tree_column, cell, model, itr, i):
+    "custom cell renderer gtype scalar"
+    info = model.get(itr, i)
+    cell.text = "" if info is None else info
+
+
+def do_text_cell_edited(renderer, text_path, new_text, slist):
+    "callback for edited signal of text cell"
+    path = Gtk.TreePath.new_from_string(text_path)
+    model = slist.get_model()
+    model[model.get_iter(path)][renderer.column] = new_text
+
+
+def do_toggled(renderer, row, slist):
+    "callback for toggled signal of boolean cell"
+    col = renderer.column
+    model = slist.get_model()
+    itr = model.iter_nth_child(None, int(row))
+    model[itr][col] = not model[itr][col]
+
+
+column_types = {
+    "hidden": {"type": str, "attr": "hidden"},
+    "text": {"type": str, "renderer": Gtk.CellRendererText(), "attr": "text"},
+    "markup": {"type": str, "renderer": Gtk.CellRendererText(), "attr": "markup"},
+    "int": {"type": int, "renderer": Gtk.CellRendererText(), "attr": "text"},
+    "double": {"type": float, "renderer": Gtk.CellRendererText(), "attr": "text"},
+    "bool": {"type": bool, "renderer": Gtk.CellRendererToggle(), "attr": "active"},
+    "scalar": {
+        "type": object,
+        "renderer": Gtk.CellRendererText(),
+        "attr": scalar_cell_renderer,
+    },
+    "pixbuf": {
+        "type": "Gtk3::Gdk::Pixbuf",
+        "renderer": Gtk.CellRendererPixbuf(),
+        "attr": "pixbuf",
+    },
+}
+
+
+class SimpleList(Gtk.TreeView):
+    "A simple interface to Gtk's complex MVC list widget"
+
+    def __init__(self, columns):
+        super().__init__()
+        if len(columns.keys()) < 1:
+            raise TypeError(
+                f"Usage: {__class__.__name__}(title=type, ...)\n"
+                + " expecting a list of column title and type name pairs.\n"
+                + " can't create a SimpleList with no columns"
+            )
+        column_info = []
+        for name, typekey in columns.items():
+            if typekey not in column_types:
+                raise TypeError(
+                    f"unknown column type '{typekey}', use one of "
+                    + ", ".join(column_types.keys())
+                )
+            if "type" not in column_types[typekey]:
+                column_types[typekey]["type"] = str
+                warn(
+                    f"column type '{typekey}' has no 'type' field; did you"
+                    + " create a custom column type incorrectly?\n"
+                    + f"limping along with '{column_types[typekey]['type']}'"
+                )
+            column_info.append(
+                {
+                    "title": name,
+                    "type": column_types[typekey]["type"],
+                    "renderer": column_types[typekey]["renderer"]
+                    if "renderer" in column_types[typekey]
+                    else None,
+                    "attr": column_types[typekey]["attr"]
+                    if "attr" in column_types[typekey]
+                    else "hidden",
+                }
+            )
+
+        model = Gtk.ListStore(*[x["type"] for x in column_info])
+        self.set_model(model)
+        i = 0
+        for col in column_info:
+            if type(col["attr"]).__name__ == "function":
+                self.insert_column_with_data_func(
+                    -1,
+                    col["title"],
+                    col["renderer"],
+                    col["attr"],
+                    i,
+                )
+                i += 1
+
+            elif col["attr"] == "hidden":  # skip hidden column
+                pass
+
+            else:
+                column = Gtk.TreeViewColumn(
+                    col["title"],
+                    col["renderer"],
+                )
+                self.append_column(column)
+                if col["attr"] == "active":
+
+                    # make boolean columns respond to editing.
+                    row = column.get_cells()[0]
+                    row.activatable = True
+                    row.connect("toggled", do_toggled, self)
+                    col["renderer"].column = i
+                    i += 1
+
+                elif col["attr"] == "text":
+                    # attach a decent 'edited' callback to any
+                    # columns using a text renderer.  we do NOT
+                    # turn on editing by default.
+
+                    row = column.get_cells()
+                    col["renderer"].connect("edited", do_text_cell_edited, self)
+                    col["renderer"].column = i
+                    i += 1
+
+    @property
+    def data(self):
+        "getter for data property"
+        return TiedList(self.get_model())
+
+    @data.setter
+    def data(self, new_data):
+        "setter for data property"
+        self.get_model().clear()
+        self.data.extend(new_data)
+
+    def set_column_editable(self, index, editable):
+        "set whether a column can be edited"
+        column = self.get_column(index)
+        if column is None:
+            raise ValueError(f"invalid column index {index}")
+        cell_renderer = column.get_cells()
+        return cell_renderer[0].set_property("editable", editable)
+
+    def get_column_editable(self, index):
+        "return whether a column can be edited"
+        column = self.get_column(index)
+        if column is None:
+            raise ValueError(f"invalid column index {index}")
+        cell_renderer = column.get_cells()
+        return cell_renderer[0].get_property("editable")
+
+    def get_selected_indices(self):
+        "get selected indices"
+        selection = self.get_selection()
+
+        # warning: this assumes that the TreeModel is actually a ListStore.
+        # if the model is a TreeStore, get_indices will return more than one
+        # index, which tells you how to get all the way down into the tree,
+        # but all the indices will be squashed into one array... so, ah,
+        # don't use this for TreeStores!
+        _model, indices = selection.get_selected_rows()
+        return [x.get_indices()[0] for x in indices]
+
+    def select(self, indices):
+        "select indices"
+        selection = self.get_selection()
+        if len(indices) > 1 and selection.get_mode() != "multiple":
+            indices = [indices[0]]
+        model = self.get_model()
+        for i in indices:
+            itr = model.iter_nth_child(None, i)
+            if not itr:
+                continue
+            selection.select_iter(itr)
+
+    def unselect(self, indices):
+        "unselect indices"
+        selection = self.get_selection()
+        if len(indices) > 1 and selection.get_mode() != "multiple":
+            indices = [indices[0]]
+        model = self.get_model()
+        for i in indices:
+            itr = model.iter_nth_child(None, i)
+            if not itr:
+                continue
+            selection.unselect_iter(itr)
+
+    def get_row_data_from_path(self, path):
+        """get row for given path
+
+        path.get_depth() always 1 for SimpleList
+        depth = path.get_depth()
+        array has only one member for SimpleList"""
+        indices = path.get_indices()
+        index = indices[0]
+        return list(self.get_model()[index])
+
+    @classmethod
+    def add_column_type(cls, **kwargs):
+        "add column type"
+        for name, typedict in kwargs.items():
+            column_types[name] = typedict
+
+    @classmethod
+    def get_column_types(cls):
+        "return column types"
+        return column_types
+
+
+class TiedRow(list):
+    "TiedRow is the lowest-level tie, allowing you to treat a row as an array of column data."
+
+    def __init__(self, model, itr):
+        super().__init__()
+        self.model = model
+        self.iter = itr
+
+    def __getitem__(self, index):
+        return self.model[self.iter][index]
+
+    def __setitem__(self, index, value):
+        if value is not None:  # allow 0, but not undef
+            self.model[self.iter][index] = value
+
+    def __len__(self):
+        return self.model.get_n_columns()
+
+    def __contains__(self, index):
+        return index < self.model.get_n_columns()
+
+    def __delitem__(self, _index):
+        raise NotImplementedError(
+            "delete called on a TiedRow, but you can't change its size"
+        )
+
+    def extend(self, _items):
+        raise NotImplementedError(
+            "extend called on a TiedRow, but you can't change its size"
+        )
+
+    def clear(self):
+        raise NotImplementedError(
+            "clear called on a TiedRow, but you can't change its size"
+        )
+
+    def pop(self):
+        raise NotImplementedError(
+            "pop called on a TiedRow, but you can't change its size"
+        )
+
+    def append(self, _item):
+        raise NotImplementedError(
+            "append called on a TiedRow, but you can't change its size"
+        )
+
+    def insert(self, _index, _item):
+        raise NotImplementedError(
+            "push called on a TiedRow, but you can't change its size"
+        )
+
+
+class TiedList(list):
+    "TiedList is an array in which each element is a row in the liststore."
+
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    def __getitem__(self, index):
+        itr = self.model.iter_nth_child(None, index)
+        if itr is None:
+            raise IndexError("list index out of range")
+        return TiedRow(self.model, itr)
+
+    def __setitem__(self, index, value):
+        itr = self.model.iter_nth_child(None, index)
+        if itr is None:
+            raise IndexError("list index out of range")
+        self.model[itr] = value
+
+    def __len__(self):
+        return len(self.model)
+
+    def __eq__(self, other):
+        return [list(x) for x in self.model] == other
+
+    def append(self, values):
+        self.model.append(values)
+
+    def extend(self, values):
+        for row in values:
+            self.model.append(row)
+
+    def insert(self, position, row):
+        self.model.insert(position, row)
+
+    def pop(self):
+        model = self.model
+        index = model.iter_n_children(None) - 1
+        itr = model.iter_nth_child(None, index)
+        if itr is None:
+            raise IndexError("pop from empty list")
+        ret = list(model[itr])
+        model.remove(itr)
+        return ret
+
+    def __delitem__(self, index):
+        model = self.model
+        itr = model.iter_nth_child(None, index)
+        if itr is None:
+            raise IndexError("list assignment index out of range")
+        model.remove(itr)
---
+++ scantpaper/tests/test_simplelist.py
@@ -0,0 +1,151 @@
+"tests for SimpleList"
+from simplelist import SimpleList
+from gi.repository import Gtk
+import pytest
+
+
+def test_basic():
+    "basic functionality tests for SimpleList"
+    slist = SimpleList({"col1": "text"})
+    assert isinstance(slist, SimpleList), "Created simplelist"
+    assert isinstance(slist.data, list), "simplelist data is a list"
+
+    slist.data.append(["row1"])
+    model = slist.get_model()
+    assert model[model.iter_nth_child(None, 0)][0] == "row1", "append"
+
+    slist.data.append(["row2"])
+    assert len(slist.data) == 2, "len"
+    assert model[model.iter_nth_child(None, 1)][0] == "row2", "append #2"
+
+    del slist.data[0]
+    assert model[model.iter_nth_child(None, 0)][0] == "row2", "del"
+
+    slist.data[0] = ["next row"]
+    assert model[model.iter_nth_child(None, 0)][0] == "next row", "setitem row"
+
+    slist.data[0][0] = "last row"
+    assert model[model.iter_nth_child(None, 0)][0] == "last row", "setitem col"
+
+    slist.data[0][0] = None
+    assert (
+        model[model.iter_nth_child(None, 0)][0] == "last row"
+    ), "setitem col ignores None"
+    assert len(slist.data[0]) == 1, "len(row)"
+    assert 0 in slist.data[0], "in (contains) row"
+
+    model[model.iter_nth_child(None, 0)][0] = "row2"
+    assert slist.data[0][0] == "row2", "getitem"
+
+    slist.data.insert(0, ["row1"])
+    assert model[model.iter_nth_child(None, 0)][0] == "row1", "insert"
+
+    with pytest.raises(ValueError):
+        slist.get_column_editable(1)
+
+    with pytest.raises(ValueError):
+        slist.set_column_editable(1, True)
+
+    assert slist.get_selected_indices() == [], "get_selected_indices"
+    slist.select([0])
+    assert slist.get_selected_indices() == [0], "select"
+    slist.select([0, 4])
+    assert slist.get_selected_indices() == [0], "select too many indices"
+    slist.select([4])
+    assert slist.get_selected_indices() == [0], "select with invalid indices"
+    slist.unselect([0])
+    assert slist.get_selected_indices() == [], "unselect"
+    slist.unselect([0, 4])
+    assert slist.get_selected_indices() == [], "unselect too many indices"
+    slist.unselect([4])
+    assert slist.get_selected_indices() == [], "unselect with invalid indices"
+
+    assert slist.get_row_data_from_path(Gtk.TreePath(0)) == [
+        "row1"
+    ], "get_row_data_from_path"
+
+    assert slist.data.pop() == ["row2"], "pop"
+    assert slist.data == [["row1"]], "data after pop"
+
+    slist.data = [["new data"]]
+    assert model[model.iter_nth_child(None, 0)][0] == "new data", "set data"
+
+
+def test_error():
+    "test error handling in SimpleList"
+
+    with pytest.raises(TypeError):
+        SimpleList({})
+
+    with pytest.raises(TypeError):
+        SimpleList({"col": None})
+
+    SimpleList.add_column_type(new={})
+    slist = SimpleList(
+        {"col1": "new", "col2": "markup", "col3": "bool", "col4": "scalar"}
+    )
+    assert (
+        slist.get_column_types()["new"]["type"] == str
+    ), "unknown custom renderers default to str"
+
+    slist = SimpleList({"col1": "text"})
+    with pytest.raises(IndexError):
+        slist.data.pop()
+
+    slist.data.append(["row1"])
+    with pytest.raises(NotImplementedError):
+        del slist.data[0][0]
+
+    with pytest.raises(NotImplementedError):
+        slist.data[0].extend([])
+
+    with pytest.raises(NotImplementedError):
+        slist.data[0].clear()
+
+    with pytest.raises(NotImplementedError):
+        slist.data[0].pop()
+
+    with pytest.raises(NotImplementedError):
+        slist.data[0].append("item")
+
+    with pytest.raises(NotImplementedError):
+        slist.data[0].insert(0, "item")
+
+    with pytest.raises(IndexError):
+        slist.data[10] = ["something"]
+
+    with pytest.raises(IndexError):
+        _ = slist.data[10]
+
+    with pytest.raises(IndexError):
+        del slist.data[10]
+
+
+def test_renderer():
+    "test renderer in SimpleList"
+    window = Gtk.Window()
+    slist = SimpleList({"col": "scalar"})
+    slist.data.append(["row1"])
+    window.add(slist)
+    window.show_all()
+    assert True, "scalar_cell_renderer() threw no error"
+
+
+def test_signals():
+    "test signals in SimpleList"
+    slist = SimpleList({"col1": "text", "col2": "bool"})
+    slist.data.append(["row1", True])
+
+    assert not slist.get_column_editable(0), "get_column_editable"
+    slist.set_column_editable(0, True)
+    assert slist.get_column_editable(0), "set_column_editable"
+
+    column = slist.get_column(0)
+    cell_renderer = column.get_cells()
+    cell_renderer[0].emit("edited", "0", "new text")
+    assert slist.data[0][0] == "new text", "edited"
+
+    column = slist.get_column(1)
+    cell_renderer = column.get_cells()
+    cell_renderer[0].emit("toggled", 0)
+    assert not slist.data[0][1], "toggled"
