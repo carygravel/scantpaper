14e4767a7ca2568912034b2547f8d85bacb310ca
Jeffrey Ratcliffe <jffry@posteo.net>
2023-10-13 00:00:00 +0000

    :test_tube: + test_1152_save_pdf_with_old_metadata.py

---
+++ scantpaper/tests/test_1152_save_pdf_with_old_metadata.py
@@ -0,0 +1,61 @@
+"Test writing PDF with old metadata"
+
+import re
+import os
+import subprocess
+import tempfile
+import pytest
+from gi.repository import GLib
+from document import Document
+
+
+@pytest.mark.skip(
+    reason="reportlab has no support for has no support for setting /CreationDate and /ModDate"
+    "https://stackoverflow.com/questions/52358853/"
+    "reportlab-metadata-creationdate-and-modificationdate"
+)
+def test_1(import_in_mainloop):
+    "Test writing PDF with old metadata"
+
+    pnm = "test.pnm"
+    pdf = "test.pdf"
+    subprocess.run(["convert", "rose:", pnm], check=True)
+
+    slist = Document()
+
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    import_in_mainloop(slist, [pnm])
+
+    metadata = {"datetime": [1966, 2, 10, 0, 0, 0], "title": "metadata title"}
+
+    def error_callback():
+        assert True, "caught errors setting timestamp"
+
+    mlp = GLib.MainLoop()
+    slist.save_pdf(
+        path=pdf,
+        list_of_pages=[slist.data[0][2]],
+        metadata=metadata,
+        options={"set_timestamp": True},
+        finished_callback=lambda response: mlp.quit(),
+        error_callback=error_callback,
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    # FIXME: If whichever library we end up has no support for has no support
+    # for setting /CreationDate and /ModDate, we can directly modify
+    # the PDF:
+    # https://stackoverflow.com/questions/52358853/reportlab-metadata-creationdate-and-modificationdate
+    info = subprocess.check_output(["pdfinfo", "-isodates", pdf], text=True)
+    assert (
+        re.search(r"1966-02-10T00:00:00Z", info) is not None
+    ), "metadata ModDate in PDF"
+
+    #########################
+
+    for fname in [pnm, pdf]:
+        if os.path.isfile(fname):
+            os.remove(fname)
