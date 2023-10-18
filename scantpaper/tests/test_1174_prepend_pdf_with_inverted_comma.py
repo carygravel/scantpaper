2d079876f4113297be5e1904780a5ae1d3954aba
Jeffrey Ratcliffe <jffry@posteo.net>
2023-10-18 00:00:00 +0000

    :white_check_mark: + test_1174_prepend_pdf_with_inverted_comma.py

---
+++ scantpaper/tests/test_1174_prepend_pdf_with_inverted_comma.py
@@ -0,0 +1,45 @@
+"Test prepending a page to a PDF"
+
+import os
+import re
+import subprocess
+import tempfile
+from gi.repository import GLib
+from document import Document
+
+
+def test_1(import_in_mainloop):
+    "Test prepending a page to a PDF"
+
+    subprocess.run(["convert", "rose:", "test.pnm"], check=True)
+    subprocess.run(["convert", "rose:", "test.tif"], check=True)
+    subprocess.run(["tiff2pdf", "-o", "te'st.pdf", "test.tif"], check=True)
+
+    slist = Document()
+
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    import_in_mainloop(slist, ["test.pnm"])
+
+    slist.save_pdf(
+        path="te'st.pdf",
+        list_of_pages=[slist.data[0][2]],
+        options={
+            "prepend": "te'st.pdf",
+        },
+        finished_callback=lambda response: mlp.quit(),
+    )
+    mlp = GLib.MainLoop()
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    capture = subprocess.check_output(["pdfinfo", "te'st.pdf"], text=True)
+    assert re.search(r"Pages:\s+2", capture) is not None, "PDF prepended"
+    assert os.path.isfile("te'st.pdf.bak"), "Backed up original"
+
+    #########################
+
+    for fname in ["test.pnm", "test.tif", "te'st.pdf", "te'st.pdf.bak"]:
+        if os.path.isfile(fname):
+            os.remove(fname)
