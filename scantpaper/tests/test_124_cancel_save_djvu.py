121bf89e4786e02def2bb15e5ad3fbd741563c3b
Jeffrey Ratcliffe <jffry@posteo.net>
2023-10-24 00:00:00 +0000

    :white_check_mark: + test_124_cancel_save_djvu.py

--- scantpaper/tests/test_124_cancel_save_djvu.py
+++ scantpaper/tests/test_124_cancel_save_djvu.py
@@ -1,14 +1,19 @@
-"Test writing PDF with downsampled image"
+"Test saving a djvu with text layer"
 
 import os
 import subprocess
 import tempfile
+import shutil
+import pytest
 from gi.repository import GLib
 from document import Document
 
 
 def test_1(import_in_mainloop):
-    "Test writing PDF with downsampled image"
+    "Test saving a djvu with text layer"
+
+    if shutil.which("cjb2") is None:
+        pytest.skip("Please install cjb2 to enable test")
 
     subprocess.run(["convert", "rose:", "test.pnm"], check=True)
 
@@ -18,6 +23,8 @@ def test_1(import_in_mainloop):
     slist.set_dir(dirname.name)
 
     import_in_mainloop(slist, ["test.pnm"])
+
+    slist.data[0][2].import_text("The quick brown fox")
 
     def finished_callback(_response):
         assert False, "Finished callback"
@@ -30,14 +37,16 @@ def test_1(import_in_mainloop):
         called = True
         mlp.quit()
 
-    slist.save_pdf(
-        path="test.pdf",
+    slist.save_djvu(
+        path="test.djvu",
         list_of_pages=[slist.data[0][2]],
         finished_callback=finished_callback,
     )
     slist.cancel(cancelled_callback)
     GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
     mlp.run()
+
+    assert called, "Cancelled callback"
 
     slist.save_image(
         path="test.jpg",
@@ -54,6 +63,6 @@ def test_1(import_in_mainloop):
 
     #########################
 
-    for fname in ["test.pnm", "test.pdf", "test.jpg"]:
+    for fname in ["test.pnm", "test.djvu", "test.jpg"]:
         if os.path.isfile(fname):
             os.remove(fname)
