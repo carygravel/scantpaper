8f2ce559a95e5e67376b30f8260539dff91b526c
Jeffrey Ratcliffe <jffry@posteo.net>
2023-10-27 00:00:00 +0000

    :white_check_mark: + test_126_save_djvu_with_error.py

---
+++ scantpaper/tests/test_126_save_djvu_with_error.py
@@ -0,0 +1,71 @@
+"Test saving a djvu and triggering an error"
+
+import os
+import subprocess
+import tempfile
+import shutil
+import pytest
+from gi.repository import GLib
+from document import Document
+
+
+def test_1(import_in_mainloop):
+    "Test saving a djvu and triggering an error"
+
+    if shutil.which("cjb2") is None:
+        pytest.skip("Please install cjb2 to enable test")
+
+    # Create test image
+    subprocess.run(["convert", "rose:", "test.pnm"], check=True)
+
+    slist = Document()
+    asserts = 0
+
+    # dir for temporary files
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    import_in_mainloop(slist, ["test.pnm"])
+
+    # inject error before save_djvu
+    os.chmod(dirname.name, 0o500)  # no write access
+
+    def error_callback1(_page, _process, _message):
+        "no write access"
+        assert True, "caught error injected before save_djvu"
+        nonlocal asserts
+        asserts += 1
+        mlp.quit()
+
+    mlp = GLib.MainLoop()
+    slist.save_djvu(
+        path="test.djvu",
+        list_of_pages=[slist.data[0][2]],
+        error_callback=error_callback1,
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    def error_callback2(_page, _process, _message):
+        assert True, "save_djvu caught error injected in queue"
+        os.chmod(dirname.name, 0o700)  # allow write access
+        nonlocal asserts
+        asserts += 1
+        mlp.quit()
+
+    mlp = GLib.MainLoop()
+    slist.save_djvu(
+        path="test.djvu",
+        list_of_pages=[slist.data[0][2]],
+        error_callback=error_callback2,
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    assert asserts == 2, "ran all callbacks"
+
+    #########################
+
+    for fname in ["test.pnm", "test.djvu"]:
+        if os.path.isfile(fname):
+            os.remove(fname)
