42e762aa567fc3cf695eb8f94568092fa7fdb51d
Jeffrey Ratcliffe <jffry@posteo.net>
2023-12-20 00:00:00 +0000

    :white_check_mark: + test_1622_import_multipage_pdf.py

---
+++ scantpaper/tests/test_1622_import_multipage_pdf.py
@@ -0,0 +1,37 @@
+"Test importing PDF"
+
+import os
+import subprocess
+import tempfile
+from gi.repository import GLib
+from document import Document
+
+
+def test_1():
+    "Test importing PDF"
+
+    subprocess.run(["convert", "rose:", "test.tif"], check=True)
+    subprocess.run(["tiffcp", "test.tif", "test.tif", "test2.tif"], check=True)
+    subprocess.run(["tiff2pdf", "-o", "test2.pdf", "test2.tif"], check=True)
+
+    slist = Document()
+
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    mlp = GLib.MainLoop()
+
+    slist.import_files(
+        paths=["test2.pdf"],
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    assert len(slist.data) == 2, "imported 2 pages"
+
+    #########################
+
+    for fname in ["test.tif", "test2.tif", "test2.pdf"]:
+        if os.path.isfile(fname):
+            os.remove(fname)
