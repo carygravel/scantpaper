4cc79e9e636f31287ce9dbe57e7de2d1e1db70f7
Jeffrey Ratcliffe <jffry@posteo.net>
2023-12-20 00:00:00 +0000

    :white_check_mark: + test_1614_import_linked_tiff.py

---
+++ scantpaper/tests/test_1614_import_linked_tiff.py
@@ -0,0 +1,43 @@
+"Test importing TIFF"
+
+import os
+import subprocess
+import tempfile
+from gi.repository import GLib
+from document import Document
+
+
+def test_1():
+    "Test importing TIFF"
+
+    subprocess.run(["convert", "rose:", "test.tif"], check=True)
+    subprocess.run(["ln", "-s", "test.tif", "test2.tif"], check=True)
+    old = subprocess.check_output(
+        ["identify", "-format", "%m %G %g %z-bit %r", "test.tif"], text=True
+    )
+
+    slist = Document()
+
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    mlp = GLib.MainLoop()
+
+    slist.import_files(
+        paths=["test2.tif"],
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    new = subprocess.check_output(
+        ["identify", "-format", "%m %G %g %z-bit %r", slist.data[0][2].filename],
+        text=True,
+    )
+    assert new == old, "TIFF imported correctly"
+
+    #########################
+
+    for fname in ["test.tif", "test2.tif"]:
+        if os.path.isfile(fname) or os.path.islink(fname):
+            os.remove(fname)
