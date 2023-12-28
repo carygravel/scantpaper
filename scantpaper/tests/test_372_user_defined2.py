6406c3227fb196178b157be49b34a1b62dce96d6
Jeffrey Ratcliffe <jffry@posteo.net>
2023-12-28 00:00:00 +0000

    :white_check_mark: + test_372_user_defined2.py

---
+++ scantpaper/tests/test_372_user_defined2.py
@@ -0,0 +1,45 @@
+"Test user-defined tools"
+
+import os
+import subprocess
+import tempfile
+from gi.repository import GLib
+from document import Document
+
+
+def test_1(import_in_mainloop):
+    "Test user-defined tools"
+
+    subprocess.run(["convert", "xc:white", "white.pnm"], check=True)
+
+    slist = Document()
+
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    import_in_mainloop(slist, ["white.pnm"])
+
+    mlp = GLib.MainLoop()
+    slist.user_defined(
+        page=slist.data[0][2],
+        command="convert %i -negate %i",
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    mlp = GLib.MainLoop()
+    slist.analyse(
+        list_of_pages=[slist.data[0][2]],
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    assert slist.data[0][2].mean == [255, 255, 255], "User-defined with %i"
+
+    #########################
+
+    for fname in ["white.pnm"]:
+        if os.path.isfile(fname):
+            os.remove(fname)
