05c60dde61f0a8d722ca1a2fb51b4accc52aeba4
Jeffrey Ratcliffe <jffry@posteo.net>
2024-01-12 00:00:00 +0000

    :white_check_mark: + test for do_save_pdf() directly

---
+++ scantpaper/tests/test_do_save_pdf.py
@@ -0,0 +1,44 @@
+"Test writing basic PDF"
+
+import os
+import re
+import subprocess
+import tempfile
+import queue
+from document import DocThread
+from basethread import Request
+from page import Page
+
+
+def test_1():
+    "Test writing basic PDF"
+
+    # Create test image
+    subprocess.run(["convert", "rose:", "test.pnm"], check=True)
+
+    thread = DocThread()
+    tdir = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    options = {
+        "dir": tdir.name,
+        "path": "test.pdf",
+        "list_of_pages": [
+            Page(
+                filename="test.pnm",
+                dir=tdir.name,
+                delete=True,
+                format="Portable anymap",
+                resolution=(70, 70, "PixelsPerInch"),
+                width=70,
+                height=46,
+            )
+        ],
+        "options": {},
+    }
+    request = Request("save_pdf", (options,), queue.Queue())
+    thread.do_save_pdf(request)
+    capture = subprocess.check_output(["pdfinfo", "test.pdf"], text=True)
+    assert re.search(r"Page size:\s+70 x 46 pts", capture), "valid PDF created"
+
+    for fname in ["test.pnm", "test.pdf"]:
+        if os.path.isfile(fname):
+            os.remove(fname)
