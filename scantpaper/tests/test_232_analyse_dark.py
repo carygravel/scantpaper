190c4cc29a1c3506cf0d61bcae0052cd9a0e622c
Jeffrey Ratcliffe <jffry@posteo.net>
2024-09-21 00:00:00 +0000

    :white_check_mark: + test_232_analyse_dark

---
+++ scantpaper/tests/test_232_analyse_dark.py
@@ -0,0 +1,39 @@
+"Test analyse"
+
+import os
+import subprocess
+import tempfile
+from gi.repository import GLib
+from document import Document
+
+
+def test_1(import_in_mainloop):
+    "Test analyse"
+
+    subprocess.run(["convert", "xc:black", "black.pnm"], check=True)
+
+    slist = Document()
+
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    import_in_mainloop(slist, ["black.pnm"])
+
+    mlp = GLib.MainLoop()
+    slist.analyse(
+        list_of_pages=[slist.data[0][2].uuid],
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    assert slist.data[0][2].mean == [0.0, 0.0, 0.0], "Found dark page"
+    assert (
+        os.path.dirname(slist.data[0][2].filename) == dirname.name
+    ), "using session directory"
+
+    #########################
+
+    for fname in ["black.pnm"]:
+        if os.path.isfile(fname):
+            os.remove(fname)
