0ea5f43d22e8da56578664eb402f3e34f9a886d2
Jeffrey Ratcliffe <jffry@posteo.net>
2024-09-21 00:00:00 +0000

    :white_check_mark: use pgm format for analyse tests to ensure single channel

--- scantpaper/tests/test_231_analyse_blank.py
+++ scantpaper/tests/test_231_analyse_blank.py
@@ -10,14 +10,14 @@ from document import Document
 def test_1(import_in_mainloop, clean_up_files):
     "Test analyse"
 
-    subprocess.run(["convert", "xc:white", "white.pnm"], check=True)
+    subprocess.run(["convert", "xc:white", "white.pgm"], check=True)
 
     slist = Document()
 
     dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
     slist.set_dir(dirname.name)
 
-    import_in_mainloop(slist, ["white.pnm"])
+    import_in_mainloop(slist, ["white.pgm"])
 
     mlp = GLib.MainLoop()
     slist.analyse(
@@ -27,11 +27,11 @@ def test_1(import_in_mainloop, clean_up_files):
     GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
     mlp.run()
 
-    assert slist.data[0][2].std_dev == [0.0, 0.0, 0.0], "Found blank page"
+    assert slist.data[0][2].std_dev == [0.0], "Found blank page"
     assert (
         os.path.dirname(slist.data[0][2].filename) == dirname.name
     ), "using session directory"
 
     #########################
 
-    clean_up_files(["white.pnm"])
+    clean_up_files(["white.pgm"])
--- scantpaper/tests/test_232_analyse_dark.py
+++ scantpaper/tests/test_232_analyse_dark.py
@@ -10,14 +10,14 @@ from document import Document
 def test_1(import_in_mainloop, clean_up_files):
     "Test analyse"
 
-    subprocess.run(["convert", "xc:black", "black.pnm"], check=True)
+    subprocess.run(["convert", "xc:black", "black.pgm"], check=True)
 
     slist = Document()
 
     dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
     slist.set_dir(dirname.name)
 
-    import_in_mainloop(slist, ["black.pnm"])
+    import_in_mainloop(slist, ["black.pgm"])
 
     mlp = GLib.MainLoop()
     slist.analyse(
@@ -27,11 +27,11 @@ def test_1(import_in_mainloop, clean_up_files):
     GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
     mlp.run()
 
-    assert slist.data[0][2].mean == [0.0, 0.0, 0.0], "Found dark page"
+    assert slist.data[0][2].mean == [0.0], "Found dark page"
     assert (
         os.path.dirname(slist.data[0][2].filename) == dirname.name
     ), "using session directory"
 
     #########################
 
-    clean_up_files(["black.pnm"])
+    clean_up_files(["black.pgm"])
