891f5c1fb5363f8158421575c5d5e987c9baaeec
Jeffrey Ratcliffe <jffry@posteo.net>
2024-10-09 00:00:00 +0000

    :globe_with_meridians: + comment about using gettext.install()

--- scantpaper/i18n.py
+++ scantpaper/i18n.py
@@ -4,5 +4,9 @@ import gettext
 import os
 
 localedir = os.path.join(os.path.abspath(os.path.dirname(__file__)), "locale")
+
+# We could replace the following with
+#    gettext.install("gscan2pdf", localedir)
+# but then would get "Undefined variable '_'"" warnings from pylint everywhere
 translate = gettext.translation("scantpaper", localedir, fallback=True)
 _ = translate.gettext
