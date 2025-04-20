1328a000b7e9b287f366671fcdb1652e14d303d7
Jeffrey Ratcliffe <jffry@posteo.net>
2025-04-20 00:00:00 +0000

    :art: group tests for save_image

--- scantpaper/tests/test_141_save_image.py
+++ scantpaper/tests/test_141_save_image.py
@@ -1,13 +1,14 @@
 "Test writing image"
 
 import re
+import os
 import subprocess
 import tempfile
 from gi.repository import GLib
 from document import Document
 
 
-def test_1(import_in_mainloop, clean_up_files):
+def test_save_image(import_in_mainloop, clean_up_files):
     "Test writing image"
 
     subprocess.run(["convert", "rose:", "test.pnm"], check=True)
@@ -49,3 +50,70 @@ def test_1(import_in_mainloop, clean_up_files):
     #########################
 
     clean_up_files(["test.pnm", "test.jpg", "test2.png"])
+
+
+def test_save_image_with_quote(import_in_mainloop, clean_up_files):
+    "Test writing image"
+
+    subprocess.run(["convert", "rose:", "test.pnm"], check=True)
+
+    slist = Document()
+
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    import_in_mainloop(slist, ["test.pnm"])
+
+    os.mkdir("te'st")
+
+    mlp = GLib.MainLoop()
+    slist.save_image(
+        path="te'st/test.jpg",
+        list_of_pages=[slist.data[0][2].uuid],
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    example = subprocess.check_output(["identify", "te'st/test.jpg"], text=True)
+    assert (
+        re.search(r"test.jpg JPEG 70x46 70x46\+0\+0 8-bit sRGB", example) is not None
+    ), "valid JPG created"
+
+    #########################
+
+    clean_up_files(["test.pnm", "te'st/test.jpg"])
+    os.rmdir("te'st")
+
+
+def test_save_image_with_ampersand(import_in_mainloop, clean_up_files):
+    "Test writing image"
+
+    subprocess.run(["convert", "rose:", "test.pnm"], check=True)
+
+    slist = Document()
+
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    import_in_mainloop(slist, ["test.pnm"])
+
+    path = "sed & awk.png"
+
+    mlp = GLib.MainLoop()
+    slist.save_image(
+        path=path,
+        list_of_pages=[slist.data[0][2].uuid],
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    example = subprocess.check_output(["identify", path], text=True)
+    assert (
+        re.search(rf"{path} PNG 70x46 70x46\+0\+0 8-bit sRGB", example) is not None
+    ), "valid JPG created"
+
+    #########################
+
+    clean_up_files(["test.pnm", path])
