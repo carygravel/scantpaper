426bc5e93e05235e3f5da0f7c79e6c84717b0b1f
Jeffrey Ratcliffe <jffry@posteo.net>
2025-04-23 00:00:00 +0000

    :art: group tests for import_djvu

--- scantpaper/tests/test_1601_import_djvu.py
+++ scantpaper/tests/test_1601_import_djvu.py
@@ -1,5 +1,7 @@
 "Test importing DjVu"
 
+import os
+import re
 import subprocess
 import tempfile
 import shutil
@@ -8,9 +10,10 @@ import pytest
 from gi.repository import GLib
 from document import Document
 from bboxtree import VERSION
+from page import Page
 
 
-def test_1(clean_up_files):
+def test_import_djvu(clean_up_files):
     "Test importing DjVu"
 
     if shutil.which("cjb2") is None:
@@ -127,3 +130,146 @@ CreationDate	"2018-12-31 13:00:00+01:00"
     #########################
 
     clean_up_files(["test.jpg", "test.djvu", "text.txt", "ann.txt"])
+
+
+def test_import_djvu_with_error(clean_up_files):
+    "Test importing DjVu"
+
+    if shutil.which("cjb2") is None:
+        pytest.skip("Please install cjb2 to enable test")
+
+    subprocess.run(["convert", "rose:", "test.jpg"], check=True)
+    subprocess.run(["c44", "test.jpg", "test.djvu"], check=True)
+
+    slist = Document()
+
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    mlp = GLib.MainLoop()
+
+    asserts = 0
+
+    def queued_cb(response):
+        nonlocal asserts
+        assert response.request.process == "get_file_info", "queued_cb"
+        asserts += 1
+
+        # inject error during import file
+        os.chmod(dirname.name, 0o500)  # no write access
+
+    def error_cb(_page, _process, message):
+        nonlocal asserts
+        assert re.search(r"^Error", message), "error_cb"
+        asserts += 1
+
+        # inject error during import file
+        os.chmod(dirname.name, 0o700)  # no write access
+
+    slist.import_files(
+        paths=["test.djvu"],
+        queued_callback=queued_cb,
+        error_callback=error_cb,
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    assert asserts == 2, "all callbacks run"
+
+    #########################
+
+    clean_up_files(["test.jpg", "test.djvu"])
+
+
+def mock_import_djvu_txt(self, _text):
+    "mock import_djvu_txt method to test error handling"
+    raise ValueError("Error parsing djvu text")
+
+
+def test_import_djvu_with_error2(monkeypatch, clean_up_files):
+    "Test importing DjVu"
+
+    if shutil.which("cjb2") is None:
+        pytest.skip("Please install cjb2 to enable test")
+
+    subprocess.run(["convert", "rose:", "test.jpg"], check=True)
+    subprocess.run(["c44", "test.jpg", "test.djvu"], check=True)
+
+    # apply the monkeypatch for Page.import_djvu_txt to mock_import_djvu_txt
+    monkeypatch.setattr(Page, "import_djvu_txt", mock_import_djvu_txt)
+
+    slist = Document()
+
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    mlp = GLib.MainLoop()
+
+    asserts = 0
+
+    def logger_cb(response):
+        nonlocal asserts
+        assert re.search(r"error", response.status), "error_cb"
+        asserts += 1
+
+    slist.import_files(
+        paths=["test.djvu"],
+        logger_callback=logger_cb,
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    assert asserts == 1, "all callbacks run"
+    assert (
+        slist.data[0][2].image_object.mode == "RGB"
+    ), "DjVu imported otherwise correctly"
+
+    #########################
+
+    clean_up_files(["test.jpg", "test.djvu"])
+
+
+def test_import_multipage_djvu(clean_up_files):
+    "Test importing multipage DjVu"
+
+    if shutil.which("cjb2") is None:
+        pytest.skip("Please install cjb2 to enable test")
+
+    subprocess.run(["convert", "rose:", "test.jpg"], check=True)
+    subprocess.run(["c44", "test.jpg", "test.djvu"], check=True)
+    subprocess.run(["djvm", "-c", "test2.djvu", "test.djvu", "test.djvu"], check=True)
+
+    slist = Document()
+
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    mlp = GLib.MainLoop()
+
+    asserts = 0
+
+    def started_cb(response):
+        nonlocal asserts
+        assert response.request.process in ["get_file_info", "import_file"]
+        asserts += 1
+
+    def error_cb(response):
+        assert False, "error thrown importing multipage djvu"
+
+    slist.import_files(
+        paths=["test2.djvu"],
+        started_callback=started_cb,
+        error_callback=error_cb,
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    assert asserts == 2, "callbacks all run"
+    assert len(slist.data) == 2, "imported 2 pages"
+
+    #########################
+
+    clean_up_files(["test.jpg", "test.djvu", "test2.djvu"])
