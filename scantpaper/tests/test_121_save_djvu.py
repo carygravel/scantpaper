15f866770efff4277bbf18b9f61a8124516bbd08
Jeffrey Ratcliffe <jffry@posteo.net>
2025-04-23 00:00:00 +0000

    :art: group tests for save_djvu

--- scantpaper/tests/test_121_save_djvu.py
+++ scantpaper/tests/test_121_save_djvu.py
@@ -1,11 +1,12 @@
 "Test saving a djvu"
 
+import codecs
+import datetime
 import os
 import re
+import shutil
 import subprocess
 import tempfile
-import shutil
-import codecs
 import pytest
 from gi.repository import GLib
 from document import Document
@@ -337,3 +338,99 @@ def test_save_djvu_different_resolutions(import_in_mainloop, clean_up_files):
     #########################
 
     clean_up_files(["test.png", "test.djvu"])
+
+
+def test_save_djvu_with_metadata(import_in_mainloop, clean_up_files):
+    "Test saving a djvu with metadata"
+
+    if shutil.which("cjb2") is None:
+        pytest.skip("Please install cjb2 to enable test")
+
+    djvu = "test.djvu"
+    pnm = "test.pnm"
+    subprocess.run(["convert", "rose:", pnm], check=True)
+
+    slist = Document()
+
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    import_in_mainloop(slist, [pnm])
+
+    metadata = {
+        "datetime": datetime.datetime(2016, 2, 10, 0, 0, tzinfo=datetime.timezone.utc),
+        "title": "metadata title",
+    }
+    slist.save_djvu(
+        path=djvu,
+        list_of_pages=[slist.data[0][2].uuid],
+        metadata=metadata,
+        options={"set_timestamp": True},
+        finished_callback=lambda response: mlp.quit(),
+    )
+    mlp = GLib.MainLoop()
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    info = subprocess.check_output(["djvused", djvu, "-e", "print-meta"], text=True)
+    assert re.search(r"metadata title", info) is not None, "metadata title in DjVu"
+    assert re.search(r"2016-02-10", info) is not None, "metadata ModDate in DjVu"
+
+    stb = os.stat(djvu)
+    assert datetime.datetime.utcfromtimestamp(stb.st_mtime) == datetime.datetime(
+        2016, 2, 10, 0, 0, 0
+    ), "timestamp"
+
+    #########################
+
+    clean_up_files([pnm, djvu])
+
+
+def test_save_djvu_with_old_metadata(import_in_mainloop, clean_up_files):
+    "Test saving a djvu with old metadata"
+
+    if shutil.which("cjb2") is None:
+        pytest.skip("Please install cjb2 to enable test")
+
+    djvu = "test.djvu"
+    pnm = "test.pnm"
+    subprocess.run(["convert", "rose:", pnm], check=True)
+
+    slist = Document()
+
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    import_in_mainloop(slist, [pnm])
+
+    called = False
+
+    def error_callback(_result):
+        nonlocal called
+        called = True
+
+    metadata = {
+        "datetime": datetime.datetime(1966, 2, 10, 0, 0, tzinfo=datetime.timezone.utc),
+        "title": "metadata title",
+    }
+    slist.save_djvu(
+        path=djvu,
+        list_of_pages=[slist.data[0][2].uuid],
+        metadata=metadata,
+        options={"set_timestamp": True},
+        finished_callback=lambda response: mlp.quit(),
+        error_callback=error_callback,
+    )
+    mlp = GLib.MainLoop()
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    assert called, "caught errors setting timestamp"
+
+    info = subprocess.check_output(["djvused", djvu, "-e", "print-meta"], text=True)
+    assert re.search(r"metadata title", info) is not None, "metadata title in DjVu"
+    assert re.search(r"1966-02-10", info) is not None, "metadata ModDate in DjVu"
+
+    #########################
+
+    clean_up_files([pnm, djvu])
