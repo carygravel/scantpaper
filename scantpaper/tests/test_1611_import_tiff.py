591c068ee1adcca7c7a3bbf18e65406d6bfe4828
Jeffrey Ratcliffe <jffry@posteo.net>
2025-04-26 00:00:00 +0000

    :art: group tests for import_tiff

--- scantpaper/tests/test_1611_import_tiff.py
+++ scantpaper/tests/test_1611_import_tiff.py
@@ -229,3 +229,51 @@ def test_import_multiple_tiffs_with_corrupt(clean_up_files):
     #########################
 
     clean_up_files([f"{i}.tif" for i in range(1, 11)])
+
+
+def test_cancel_import_tiff(import_in_mainloop, clean_up_files):
+    "Test importing TIFF"
+
+    subprocess.run(["convert", "rose:", "test.tif"], check=True)
+    subprocess.check_output(
+        ["identify", "-format", "%m %G %g %z-bit %r", "test.tif"], text=True
+    )
+
+    slist = Document()
+
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    mlp = GLib.MainLoop()
+
+    asserts = 0
+
+    def finished_cb(response):
+        assert False, "TIFF not imported"
+        mlp.quit()
+
+    def cancelled_cb(response):
+        nonlocal asserts
+        assert len(slist.data) == 0, "TIFF not imported"
+        asserts += 1
+        mlp.quit()
+
+    slist.import_files(
+        paths=["test.tif"],
+        finished_callback=finished_cb,
+    )
+    slist.cancel(cancelled_cb)
+
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    assert asserts == 1, "all callbacks run"
+
+    import_in_mainloop(slist, ["test.tif"])
+    assert (
+        slist.data[0][2].image_object.mode == "RGB"
+    ), "TIFF imported correctly after cancelling previous import"
+
+    #########################
+
+    clean_up_files(["test.tif"])
