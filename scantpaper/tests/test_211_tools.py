4f87952e37f884d09480f848eda6a1c7907ef6e8
Jeffrey Ratcliffe <jffry@posteo.net>
2025-04-26 00:00:00 +0000

    :art: group tests for tools

---
+++ scantpaper/tests/test_211_tools.py
@@ -0,0 +1,492 @@
+"Test rotating"
+
+import re
+import subprocess
+import tempfile
+from gi.repository import GLib
+from document import Document
+from page import VERSION
+
+
+def test_rotate(import_in_mainloop, clean_up_files):
+    "Test rotating"
+
+    subprocess.run(["convert", "rose:", "test.jpg"], check=True)
+
+    slist = Document()
+
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    import_in_mainloop(slist, ["test.jpg"])
+    slist.data[0][2].saved = True
+
+    asserts = 0
+
+    def display_cb(response):
+        nonlocal asserts
+        assert True, "Triggered display callback"
+        asserts += 1
+
+    mlp = GLib.MainLoop()
+    slist.rotate(
+        angle=90,
+        page=slist.data[0][2].uuid,
+        display_callback=display_cb,
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    assert asserts == 1, "all callbacks run"
+    assert slist.data[0][2].image_object.mode == "RGB", "valid JPG created"
+    assert not slist.scans_saved(), "modification removed saved tag"
+
+    #########################
+
+    clean_up_files(["test.jpg"])
+
+
+def test_analyse_blank(import_in_mainloop, clean_up_files):
+    "Test analyse"
+
+    subprocess.run(["convert", "-size", "10x10", "xc:white", "white.pgm"], check=True)
+
+    slist = Document()
+
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    import_in_mainloop(slist, ["white.pgm"])
+
+    mlp = GLib.MainLoop()
+    slist.analyse(
+        list_of_pages=[slist.data[0][2].uuid],
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    assert slist.data[0][2].std_dev == [0.0], "Found blank page"
+
+    #########################
+
+    clean_up_files(["white.pgm"])
+
+
+def test_analyse_dark(import_in_mainloop, clean_up_files):
+    "Test analyse"
+
+    subprocess.run(["convert", "xc:black", "black.pgm"], check=True)
+
+    slist = Document()
+
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    import_in_mainloop(slist, ["black.pgm"])
+
+    mlp = GLib.MainLoop()
+    slist.analyse(
+        list_of_pages=[slist.data[0][2].uuid],
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    assert slist.data[0][2].mean == [0.0], "Found dark page"
+
+    #########################
+
+    clean_up_files(["black.pgm"])
+
+
+def test_threshold(import_in_mainloop, clean_up_files):
+    "Test threshold"
+
+    subprocess.run(["convert", "rose:", "test.jpg"], check=True)
+
+    slist = Document()
+
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    import_in_mainloop(slist, ["test.jpg"])
+    slist.data[0][2].saved = True
+    slist.data[0][2].bboxtree = (
+        '[{"bbox":["0","0","783","1057"],"id":"page_1",'
+        '"type":"page","depth":0},{"depth":1,"id":"word_1_2","type":"word",'
+        '"confidence":"93","text":"ACCOUNT","bbox":["218","84","401","109"]}]'
+    )
+
+    mlp = GLib.MainLoop()
+    slist.threshold(
+        threshold=80,
+        page=slist.data[0][2].uuid,
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    mlp = GLib.MainLoop()
+    slist.analyse(
+        list_of_pages=[slist.data[0][2].uuid],
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    assert len(slist.data[0][2].mean) == 1, "depth == 1"
+    assert re.search("ACCOUNT", slist.data[0][2].bboxtree), "OCR output still there"
+    assert not slist.scans_saved(), "modification removed saved tag"
+
+    #########################
+
+    clean_up_files(["test.jpg"])
+
+
+def test_negate(import_in_mainloop, clean_up_files):
+    "Test negate"
+
+    subprocess.run(["convert", "xc:white", "white.pnm"], check=True)
+
+    slist = Document()
+
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    import_in_mainloop(slist, ["white.pnm"])
+    slist.data[0][2].saved = True
+    slist.data[0][2].bboxtree = (
+        '[{"bbox":["0","0","783","1057"],"id":"page_1",'
+        '"type":"page","depth":0},{"depth":1,"id":"word_1_2","type":"word",'
+        '"confidence":"93","text":"ACCOUNT","bbox":["218","84","401","109"]}]'
+    )
+
+    mlp = GLib.MainLoop()
+    slist.analyse(
+        list_of_pages=[slist.data[0][2].uuid],
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+    assert min(slist.data[0][2].mean) == 255, "mean before"
+
+    mlp = GLib.MainLoop()
+    slist.negate(
+        page=slist.data[0][2].uuid,
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    mlp = GLib.MainLoop()
+    slist.analyse(
+        list_of_pages=[slist.data[0][2].uuid],
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    assert max(slist.data[0][2].mean) == 0, "mean afterwards"
+    assert re.search("ACCOUNT", slist.data[0][2].bboxtree), "OCR output still there"
+    assert not slist.scans_saved(), "modification removed saved tag"
+
+    #########################
+
+    clean_up_files(["white.pnm"])
+
+
+def test_unsharp_mask(import_in_mainloop, clean_up_files):
+    "Test unsharp mask"
+
+    subprocess.run(["convert", "rose:", "test.jpg"], check=True)
+
+    slist = Document()
+
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    import_in_mainloop(slist, ["test.jpg"])
+    slist.data[0][2].saved = True
+    slist.data[0][2].bboxtree = (
+        '[{"bbox":["0","0","783","1057"],"id":"page_1",'
+        '"type":"page","depth":0},{"depth":1,"id":"word_1_2","type":"word",'
+        '"confidence":"93","text":"ACCOUNT","bbox":["218","84","401","109"]}]'
+    )
+
+    mlp = GLib.MainLoop()
+    slist.analyse(
+        list_of_pages=[slist.data[0][2].uuid],
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+    assert slist.data[0][2].mean == [
+        145.5391304347826,
+        89.22546583850932,
+        80.40186335403726,
+    ], "mean before"
+
+    mlp = GLib.MainLoop()
+    slist.unsharp(
+        radius=1,
+        percent=200,
+        threshold=3,
+        page=slist.data[0][2].uuid,
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    mlp = GLib.MainLoop()
+    slist.analyse(
+        list_of_pages=[slist.data[0][2].uuid],
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    assert slist.data[0][2].mean != [
+        145.5391304347826,
+        89.22546583850932,
+        80.40186335403726,
+    ], "mean after"
+    assert re.search("ACCOUNT", slist.data[0][2].bboxtree), "OCR output still there"
+    assert not slist.scans_saved(), "modification removed saved tag"
+
+    #########################
+
+    clean_up_files(["test.jpg"])
+
+
+def test_crop(import_in_mainloop, clean_up_files):
+    "Test brightness contrast"
+
+    subprocess.run(["convert", "rose:", "test.gif"], check=True)
+
+    slist = Document()
+
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    import_in_mainloop(slist, ["test.gif"])
+
+    assert slist.data[0][2].width == 70, "width before crop"
+    assert slist.data[0][2].height == 46, "height before crop"
+
+    slist.data[0][2].saved = True
+    hocr = """<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+ <head>
+  <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+  <meta name='ocr-system' content='gscan2pdf 2.7.0' />
+  <meta name='ocr-capabilities' content='ocr_page ocr_carea ocr_par ocr_line ocr_word'/>
+ </head>
+ <body>
+  <div class='ocr_page' title='bbox 0 0 70 46'>
+      <span class='ocr_word' title='bbox 1 1 9 9'>beyond br</span>
+      <span class='ocr_word' title='bbox 5 5 15 15'>on br</span>
+      <span class='ocr_word' title='bbox 11 11 19 19'>inside</span>
+      <span class='ocr_word' title='bbox 15 15 25 25'>on tl</span>
+      <span class='ocr_word' title='bbox 21 21 29 29'>beyond tl</span>
+  </div>
+ </body>
+</html>
+"""
+    slist.data[0][2].import_hocr(hocr)
+
+    mlp = GLib.MainLoop()
+    slist.crop(
+        x=10,
+        y=10,
+        w=10,
+        h=10,
+        page=slist.data[0][2].uuid,
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+    assert slist.data[0][2].width == 10, "page width after crop"
+    assert slist.data[0][2].height == 10, "page height after crop"
+    image = slist.data[0][2].image_object
+    assert image.width == 10, "image width after crop"
+    assert image.height == 10, "image height after crop"
+    hocr = f"""<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+ <head>
+  <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+  <meta name='ocr-system' content='gscan2pdf {VERSION}' />
+  <meta name='ocr-capabilities' content='ocr_page ocr_carea ocr_par ocr_line ocr_word'/>
+ </head>
+ <body>
+  <div class='ocr_page' title='bbox 0 0 10 10'>
+   <span class='ocr_word' title='bbox 0 0 5 5'>on br</span>
+   <span class='ocr_word' title='bbox 1 1 9 9'>inside</span>
+   <span class='ocr_word' title='bbox 5 5 10 10'>on tl</span>
+  </div>
+ </body>
+</html>
+"""
+    assert slist.data[0][2].export_hocr() == hocr, "cropped hocr"
+    assert not slist.scans_saved(), "modification removed saved tag"
+
+    #########################
+
+    clean_up_files(["test.gif"])
+
+
+def test_split(import_in_mainloop, clean_up_files):
+    "Test brightness contrast"
+
+    subprocess.run(["convert", "rose:", "test.gif"], check=True)
+
+    slist = Document()
+
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    import_in_mainloop(slist, ["test.gif"])
+
+    assert slist.data[0][2].width == 70, "width before crop"
+    assert slist.data[0][2].height == 46, "height before crop"
+
+    slist.data[0][2].saved = True
+    hocr = """<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+ <head>
+  <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+  <meta name='ocr-system' content='gscan2pdf 2.7.0' />
+  <meta name='ocr-capabilities' content='ocr_page ocr_carea ocr_par ocr_line ocr_word'/>
+ </head>
+ <body>
+  <div class='ocr_page' title='bbox 0 0 70 46'>
+   <span class='ocr_word' title='bbox 0 0 9 46'>left</span>
+   <span class='ocr_word' title='bbox 10 0 45 46'>middle</span>
+   <span class='ocr_word' title='bbox 46 0 70 46'>right</span>
+  </div>
+ </body>
+</html>
+"""
+    slist.data[0][2].import_hocr(hocr)
+
+    mlp = GLib.MainLoop()
+    slist.split_page(
+        direction="v",
+        position=35,
+        page=slist.data[0][2].uuid,
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+    assert slist.data[0][2].width == 35, "1st page width after split"
+    assert slist.data[0][2].height == 46, "1st page height after split"
+    image = slist.data[0][2].image_object
+    assert image.width == 35, "1st image width after crop"
+    assert image.height == 46, "1st image height after crop"
+    hocr = f"""<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+ <head>
+  <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+  <meta name='ocr-system' content='gscan2pdf {VERSION}' />
+  <meta name='ocr-capabilities' content='ocr_page ocr_carea ocr_par ocr_line ocr_word'/>
+ </head>
+ <body>
+  <div class='ocr_page' title='bbox 0 0 35 46'>
+   <span class='ocr_word' title='bbox 0 0 9 46'>left</span>
+   <span class='ocr_word' title='bbox 10 0 35 46'>middle</span>
+  </div>
+ </body>
+</html>
+"""
+    assert slist.data[0][2].export_hocr() == hocr, "split hocr"
+
+    assert slist.data[1][2].width == 35, "2nd page width after split"
+    assert slist.data[1][2].height == 46, "2nd page height after split"
+    image = slist.data[1][2].image_object
+    assert image.width == 35, "2nd image width after crop"
+    assert image.height == 46, "2nd image height after crop"
+    hocr = f"""<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+ <head>
+  <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+  <meta name='ocr-system' content='gscan2pdf {VERSION}' />
+  <meta name='ocr-capabilities' content='ocr_page ocr_carea ocr_par ocr_line ocr_word'/>
+ </head>
+ <body>
+  <div class='ocr_page' title='bbox 0 0 35 46'>
+   <span class='ocr_word' title='bbox 0 0 10 46'>middle</span>
+   <span class='ocr_word' title='bbox 11 0 35 46'>right</span>
+  </div>
+ </body>
+</html>
+"""
+    assert slist.data[1][2].export_hocr() == hocr, "split hocr"
+
+    assert not slist.scans_saved(), "modification removed saved tag"
+
+    #########################
+
+    clean_up_files(["test.gif"])
+
+
+def test_brightness_contrast(import_in_mainloop, clean_up_files):
+    "Test brightness contrast"
+
+    subprocess.run(["convert", "rose:", "test.jpg"], check=True)
+
+    slist = Document()
+
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    import_in_mainloop(slist, ["test.jpg"])
+    slist.data[0][2].saved = True
+    slist.data[0][2].bboxtree = (
+        '[{"bbox":["0","0","783","1057"],"id":"page_1",'
+        '"type":"page","depth":0},{"depth":1,"id":"word_1_2","type":"word",'
+        '"confidence":"93","text":"ACCOUNT","bbox":["218","84","401","109"]}]'
+    )
+    mlp = GLib.MainLoop()
+    slist.analyse(
+        list_of_pages=[slist.data[0][2].uuid],
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+    mean = [145.5391304347826, 89.22546583850932, 80.40186335403726]
+    assert slist.data[0][2].mean == mean, "mean before"
+
+    mlp = GLib.MainLoop()
+    slist.brightness_contrast(
+        brightness=65,
+        contrast=65,
+        page=slist.data[0][2].uuid,
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+    mlp = GLib.MainLoop()
+    slist.analyse(
+        list_of_pages=[slist.data[0][2].uuid],
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+    assert slist.data[0][2].mean != mean, "mean after"
+    assert re.search("ACCOUNT", slist.data[0][2].bboxtree), "OCR output still there"
+    assert not slist.scans_saved(), "modification removed saved tag"
+
+    #########################
+
+    clean_up_files(["test.jpg"])
