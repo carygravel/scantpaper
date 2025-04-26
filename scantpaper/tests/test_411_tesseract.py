410ad8522b00945feecf80a4e204d9306b049aa8
Jeffrey Ratcliffe <jffry@posteo.net>
2025-04-26 00:00:00 +0000

    :art: group tests for tesseract

--- scantpaper/tests/test_411_tesseract.py
+++ scantpaper/tests/test_411_tesseract.py
@@ -2,7 +2,11 @@
 
 import re
 import shutil
+import subprocess
+import tempfile
 import pytest
+from gi.repository import GLib
+from document import Document
 from tesseract import languages, _iso639_1to3, locale_installed, get_tesseract_codes
 
 
@@ -33,3 +37,51 @@ def test_tesseract_code_conversions():
 def test_get_tesseract_codes():
     "test get_tesseract_codes()"
     assert isinstance(get_tesseract_codes(), list), "get_tesseract_codes() returns list"
+
+
+def test_tesseract_in_thread(import_in_mainloop, clean_up_files):
+    "Test importing PDF"
+
+    args = [
+        "convert",
+        "+matte",
+        "-depth",
+        "1",
+        "-colorspace",
+        "Gray",
+        "-family",
+        "DejaVu Sans",
+        "-pointsize",
+        "12",
+        "-density",
+        "300",
+        "label:The quick brown fox",
+        "test.png",
+    ]
+    subprocess.run(args, check=True)
+
+    slist = Document()
+
+    dirname = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+    slist.set_dir(dirname.name)
+
+    import_in_mainloop(slist, ["test.png"])
+
+    mlp = GLib.MainLoop()
+    slist.tesseract(
+        page=slist.data[0][2].uuid,
+        language="eng",
+        finished_callback=lambda response: mlp.quit(),
+    )
+    GLib.timeout_add(2000, mlp.quit)  # to prevent it hanging
+    mlp.run()
+
+    hocr = slist.data[0][2].export_hocr()
+    assert re.search(r"T[hn]e", hocr), 'Tesseract returned "The"'
+    assert re.search(r"quick", hocr), 'Tesseract returned "quick"'
+    assert re.search(r"brown", hocr), 'Tesseract returned "brown"'
+    assert re.search(r"f(o|0)x", hocr), 'Tesseract returned "fox"'
+
+    #########################
+
+    clean_up_files(["test.png"])
