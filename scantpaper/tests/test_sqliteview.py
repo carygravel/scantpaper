43fdb86d5a4a57067411191ce9adf04399efe4f3
Jeffrey Ratcliffe <jffry@posteo.net>
2025-05-05 00:00:00 +0000

    :sparkles: use SQLite to generate unique page ids

--- scantpaper/tests/test_sqliteview.py
+++ scantpaper/tests/test_sqliteview.py
@@ -29,29 +29,29 @@ def test_basic(clean_up_files):
     assert len(view.data) == 0, "len emty data"
 
     pixbuf = rose()
-    view.data.append([1, pixbuf])
+    view.data.append([1, pixbuf, 1])
     model = view.get_model()
     assert model[model.iter_nth_child(None, 0)][0] == 1, "append"
     assert len(view.data) == 1, "len"
 
-    view.data.append([2, pixbuf])
+    view.data.append([2, pixbuf, 2])
     del view.data[0]
     assert model[model.iter_nth_child(None, 0)][0] == 2, "del"
 
-    view.data[0] = [3, pixbuf]
+    view.data[0] = [3, pixbuf, 3]
     assert model[model.iter_nth_child(None, 0)][0] == 3, "setitem row"
 
     view.data[0][0] = 4
     assert model[model.iter_nth_child(None, 0)][0] == 4, "setitem col"
 
     view.data[0][0] = None
-    assert len(view.data[0]) == 2, "len(row)"
+    assert len(view.data[0]) == 3, "len(row)"
     assert 0 in view.data[0], "in (contains) row"
 
     model[model.iter_nth_child(None, 0)][0] = 5
     assert view.data[0][0] == 5, "getitem"
 
-    view.data.insert(0, [6, pixbuf])
+    view.data.insert(0, [6, pixbuf, 6])
     assert model[model.iter_nth_child(None, 0)][0] == 6, "insert"
 
     assert view.get_selected_indices() == [], "get_selected_indices"
@@ -81,7 +81,7 @@ def test_basic(clean_up_files):
     assert view.data.pop()[0] == 5, "pop"
     assert view.data[0][0] == 6, "data after pop"
 
-    view.data = [[1, pixbuf]]
+    view.data = [[1, pixbuf, 1]]
     assert model[model.iter_nth_child(None, 0)][0] == 1, "set data"
 
     clean_up_files([Path(tempfile.gettempdir()) / "document.db"])
@@ -92,7 +92,7 @@ def test_iterators(clean_up_files):
 
     view = SqliteView()
     pixbuf = rose()
-    view.data = [[1, pixbuf]]
+    view.data = [[1, pixbuf, 1]]
     flag = False
     for _row in view.data:
         flag = True
@@ -120,7 +120,7 @@ def test_error(clean_up_files):
         view.data.pop()
 
     pixbuf = rose()
-    view.data.append([1, pixbuf])
+    view.data.append([1, pixbuf, 1])
     with pytest.raises(NotImplementedError):
         del view.data[0][0]
 
@@ -156,7 +156,7 @@ def test_renderer(clean_up_files):
     window = Gtk.Window()
     view = SqliteView()
     pixbuf = rose()
-    view.data.append([1, pixbuf])
+    view.data.append([1, pixbuf, 1])
     window.add(view)
     window.show_all()
     assert True, "cell_renderer() threw no error"
@@ -168,7 +168,7 @@ def test_signals(clean_up_files):
     "test signals in SqliteView"
     view = SqliteView()
     pixbuf = rose()
-    view.data.append([1, pixbuf])
+    view.data.append([1, pixbuf, 1])
 
     assert not view.get_column_editable(0), "get_column_editable"
     view.set_column_editable(0, True)
