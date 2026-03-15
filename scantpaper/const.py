68a569a14685cb08754b1b5853f36df0fec8a25b
Jeffrey Ratcliffe <jffry@posteo.net>
2026-03-15 00:00:00 +0000

    :bookmark: extract version from metadata

--- scantpaper/const.py
+++ scantpaper/const.py
@@ -1,10 +1,25 @@
 "Constants that are used in multiple places"
 
+import importlib.metadata
+from pathlib import Path
+import tomllib
+
 PROG_NAME = "scantpaper"
 AUTHOR = "Jeffrey Ratcliffe"
 AUTHOR_EMAIL = "jffry@posteo.net"
 URL = "https://github.com/carygravel/scantpaper"
-BUG_URL = "https://github.com/carygravel/scantpaper/issues"
+BUG_URL = URL + "/issues"
+
+
+def get_version():
+    "Get version from pyproject.toml"
+    tomlfile_path = Path(__file__).parent.parent / "pyproject.toml"
+    if tomlfile_path.is_file():
+        return tomllib.loads(tomlfile_path.read_text())["project"]["version"]
+    return importlib.metadata.version(PROG_NAME)
+
+
+VERSION = get_version()
 
 ASTERISK = "*"
 DOT = "."
@@ -22,8 +37,6 @@ MM_PER_CM = 10
 MM_PER_INCH = CM_PER_INCH * MM_PER_CM
 POINTS_PER_INCH = 72
 MAX_DPI = 2400
-
-VERSION = "3.0.0"
 
 DRAGGER_TOOL = "dragger"
 SELECTOR_TOOL = "selector"
