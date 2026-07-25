c4428fbbef67380468d538e594312df58f85da0e
Jeffrey Ratcliffe <jffry@posteo.net>
2026-07-25 00:00:00 +0000

    :memo: + release_procedure.md

---
+++ release_procedure.md
@@ -0,0 +1,66 @@
+# Release procedure:
+
+1. Test scan in lineart, greyscale and colour.
+1. New screendump required? Print screen creates screenshot.png in Desktop.
+1. Download new translations (https://translations.launchpad.net/scantpaper)
+1. Update translators in credits (https://launchpad.net/scantpaper/+topcontributors)
+1. Update version in pyproject.toml
+1. Upload .pot
+   ```sh
+   python3 dev/generate_pot.py
+   ```
+1. Tag the release
+   ```sh
+   git status
+   git tag vx.x.x
+   git push --tags
+   ```
+1. Build package for Debian. Update the salsa repo:
+   ```sh
+   gbp import-orig --pristine-tar --uscan
+   #tox -e signed_sdist
+   #sudo sbuild-update -udr sid-amd64-sbuild
+   ```
+1. Make appropriate updates to debian/changelog
+   ```sh
+   debuild -S -sa
+   # or
+   sbuild -sc sid-amd64-sbuild
+   debsign .changes
+   # then
+   lintian -iI --pedantic .changes
+   autopkgtest .changes -- unshare --release sid
+   # check contents with dpkg-deb --contents
+   # test dist sudo dpkg -i scantpaper_x.x.x_all.deb
+   dput ftp-master .changes
+   ```
+1. Push changes to salsa:
+   ```sh
+   git add -p
+   debcommit -r
+   git push --set-upstream git@salsa.debian.org:python-team/packages/scantpaper.git : --tags
+   ```
+1. Build packages for Ubuntu
+
+   Name the release -0~ppa1<release>, where release (https://wiki.ubuntu.com/Releases) is:
+   - resolute (until 2031-05) - dh13
+   - noble (until 2029-06) - dh13
+   - jammy (until 2027-06)
+
+   ```sh
+   debuild -S -sa
+   dput gscan2pdf-ppa .changes
+   ```
+
+   Watch them [build](https://launchpad.net/~jeffreyratcliffe/+archive).
+1. gscan2pdf-announce@lists.sourceforge.net, gscan2pdf-help@lists.sourceforge.net,
+   sane-devel@lists.alioth.debian.org
+1. To interactively debug in the schroot:
+   Duplicate the config file, typically in /etc/schroot/chroot.d/, changing
+   the sbuild profile to desktop
+   ```sh
+   schroot -c sid-amd64-desktop -u root
+   apt-get build-dep scantpaper
+   su - <user>
+   pytest
+   ```
--- scantpaper/app.py
+++ scantpaper/app.py
@@ -1,4 +1,4 @@
-"main application"
+"scantpaper --- to aid the scan to PDF or DjVu process"
 
 # TODO:
 # change page numbering to always run from 1-n with no gaps
@@ -11,60 +11,6 @@
 # add type hints and turn on type checks in tox.ini
 # migrate to Gtk4
 # remaining FIXMEs and TODOs
-
-# scantpaper --- to aid the scan to PDF or DjVu process
-
-# Release procedure:
-#    Use
-#      pytest -vv
-#    immediately before release so as not to affect any patches
-#    in between, and then consistently before each commit afterwards.
-# 0. Test scan in lineart, greyscale and colour.
-# 1. New screendump required? Print screen creates screenshot.png in Desktop.
-#    Download new translations (https://translations.launchpad.net/scantpaper)
-#    Update translators in credits (https://launchpad.net/scantpaper/+topcontributors)
-#    Update version in pyproject.toml
-# 2. Upload .pot
-#     python3 dev/generate_pot.py
-# 3.  git status
-#     git tag vx.x.x
-#     git push --tags
-# 4. Build package for Debian. Update the salsa repo:
-#     gbp import-orig --pristine-tar --uscan
-#     #tox -e signed_sdist
-#     #sudo sbuild-update -udr sid-amd64-sbuild
-#    Make appropriate updates to debian/changelog
-#     debuild -S -sa
-#      or
-#     sbuild -sc sid-amd64-sbuild
-#     debsign .changes
-#      then
-#     lintian -iI --pedantic .changes
-#     autopkgtest .changes -- unshare --release sid
-#    check contents with dpkg-deb --contents
-#    test dist sudo dpkg -i scantpaper_x.x.x_all.deb
-#     dput ftp-master .changes
-#    Push changes to salsa:
-#     git add -p
-#     debcommit -r
-#     git push --set-upstream git@salsa.debian.org:python-team/packages/scantpaper.git : --tags
-# 5. Build packages for Ubuntu
-#    name the release -0~ppa1<release>, where release (https://wiki.ubuntu.com/Releases) is:
-#      * resolute (until 2031-05) - dh13
-#      * noble (until 2029-06) - dh13
-#      * jammy (until 2027-06)
-#     debuild -S -sa
-#     dput gscan2pdf-ppa .changes
-#    https://launchpad.net/~jeffreyratcliffe/+archive
-# 6. gscan2pdf-announce@lists.sourceforge.net, gscan2pdf-help@lists.sourceforge.net,
-#    sane-devel@lists.alioth.debian.org
-# 7. To interactively debug in the schroot:
-#      * duplicate the config file, typically in /etc/schroot/chroot.d/, changing
-#        the sbuild profile to desktop
-#       schroot -c sid-amd64-desktop -u root
-#       apt-get build-dep scantpaper
-#       su - <user>
-#       pytest -vv
 
 import argparse
 import atexit
