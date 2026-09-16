baae9bcedc1bcbf56f5a392de482a1c74120a010
Jeffrey Ratcliffe <jffry@posteo.net>
2026-09-16 17:58:49 +0200

    Revert ":loud_sound: more instrumentation around displaying the image"
    This reverts commit 43d53923daf685925433c9bd385968f5e01f31d1.

--- src/scantpaper/imageview.py
+++ src/scantpaper/imageview.py
@@ -1,6 +1,5 @@
 """Image viewer widget that can zoom, pan, select."""
 
-import logging
 from typing import ClassVar
 
 import cairo
@@ -15,8 +14,6 @@ from gi.repository import (  # noqa: E402
     GObject,
     Gtk,
 )
-
-logger = logging.getLogger(__name__)
 
 
 class Tool:
@@ -514,25 +511,6 @@ class ImageView(Gtk.DrawingArea):
         style = self.get_style_context()
         pixbuf = self.get_pixbuf()
         ratio = self.get_resolution_ratio()
-        if pixbuf is None or not hasattr(pixbuf, "get_width"):
-            logger.debug(
-                "DISPLAY do_draw pixbuf=%r zoom=%s fit=%s alloc=%sx%s",
-                pixbuf,
-                self.get_zoom(),
-                self.zoom_is_fit,
-                allocation.width,
-                allocation.height,
-            )
-        else:
-            logger.debug(
-                "DISPLAY do_draw pixbuf=%sx%s zoom=%s fit=%s alloc=%sx%s",
-                pixbuf.get_width(),
-                pixbuf.get_height(),
-                self.get_zoom(),
-                self.zoom_is_fit,
-                allocation.width,
-                allocation.height,
-            )
         style.add_class("imageview")
         style.save()
         style.add_class(Gtk.STYLE_CLASS_BACKGROUND)
@@ -573,13 +551,7 @@ class ImageView(Gtk.DrawingArea):
         context.translate(offset.x, offset.y)
         surface = self._get_or_create_surface(pixbuf)
         context.set_source_surface(surface, 0, 0)
-        ifilter = self._get_adaptive_filter()
-        logger.debug(
-            "DISPLAY do_draw rendering filter=%s interacting=%s",
-            ifilter,
-            self.get_interacting(),
-        )
-        context.get_source().set_filter(ifilter)
+        context.get_source().set_filter(self._get_adaptive_filter())
         context.paint()
         context.restore()
 
@@ -683,15 +655,6 @@ class ImageView(Gtk.DrawingArea):
 
     def set_pixbuf(self, pixbuf, *, zoom_to_fit=False):
         """Set pixbuf, optionally zooming to fit."""
-        if pixbuf is None or not hasattr(pixbuf, "get_width"):
-            logger.debug("DISPLAY set_pixbuf %r zoom_to_fit=%s", pixbuf, zoom_to_fit)
-        else:
-            logger.debug(
-                "DISPLAY set_pixbuf %sx%s zoom_to_fit=%s",
-                pixbuf.get_width(),
-                pixbuf.get_height(),
-                zoom_to_fit,
-            )
         self.pixbuf = pixbuf
         self._cached_surface = None
         self._cached_pixbuf_id = id(pixbuf) if pixbuf else None
@@ -808,16 +771,6 @@ class ImageView(Gtk.DrawingArea):
             min(sc_factor_w, sc_factor_h) * additional_factor * self.get_scale_factor(),
             (box.x + box.width / 2) / ratio,
             box.y + box.height / 2,
-        )
-        logger.debug(
-            "DISPLAY zoom_to_box box=%sx%s alloc=%sx%s ratio=%s limit=%s -> zoom=%s",
-            box.width,
-            box.height,
-            allocation.width,
-            allocation.height,
-            ratio,
-            limit,
-            self.get_zoom(),
         )
 
     def zoom_to_selection(self, context_factor):
