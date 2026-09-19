ca8d53d57b520495b5adf471a9fb2acc010d3d8f
Jeffrey Ratcliffe <jffry@posteo.net>
2026-09-19 00:00:00 +0000

    :wrench: remove tox.ini

--- src/scantpaper/app.py
+++ src/scantpaper/app.py
@@ -1,7 +1,6 @@
 """scantpaper --- to aid the scan to PDF or DjVu process."""
 
 # Roadmap and wishlist:
-# Do we need tox.ini ?
 # refactor methods using self.slist.clipboard
 # refactor ocr & annotation manipulation into single class
 # migrate to Gtk4
